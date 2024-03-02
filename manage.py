9a595d8bd54721b5b8db23107b0f5af9b6cbf9e9
Jakub Jureczko <jakub.j.jureczko@gmail.com>
2024-03-02 13:11:40 +0100

    init project

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aiproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_proj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
